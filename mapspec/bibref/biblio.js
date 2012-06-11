849eca51698556b368dcc2962578af59568abd16
AlasdairGray <A.Gray@cs.man.ac.uk>
2012-06-11 12:31:17 +0100

    Started converting mapping specification into open PHACTS specification

--- mapspec/bibref/biblio.js
+++ mapspec/bibref/biblio.js
@@ -1,2 +1,17 @@
 berjon.biblio = {
+	"DCTERMS": "DCMI Metadata Terms, Dublin Core Metadata Initiative, 11 October 2010. <a href=\"http://dublincore.org/documents/2010/10/11/dcmi-terms/\">http://dublincore.org/documents/2010/10/11/dcmi-terms/</a>",
+	"Chem2Bio2RDF": "B. Chen, X. Dong, D. Jiao, H. Wang, Q. Zhu, Y. Ding, and D.J. Wild. Chem2Bio2RDF: a semantic framework for linking and data mining chemogenomic and systems chemical biology data. BMC Bioinformatics, 11(1):255, 2010.",
+	"IMS": "Identity Mapping Service, <a href=\"https://wiki.openphacts.org/index.php/IMS_API\">https://wiki.openphacts.org/index.php/IMS_API</a>",
+	"OPM": " ",
+	"OPS-ARCH": "Lee Harland, Paul Groth, Steve Pettifer, Christine Chichester, and Antony Williams. Provide a documented first functional design and a detailed technical design of the first version of OPS. Open PHACTS Deliverable D3.2.3, version 1.0, August 2011.",
+	"OPS-IV": "C. Brenninkmeijer, C. Evelo, C. Goble, A. J. G. Gray, A. Waagmeester, and E. Willighagen. Identity and Vocabulary Services in the Open Pharmacological Space. Open PHACT draft specification, version 0.1, 3 February 2012. <a href=\"https://docs.google.com/document/d/1XSrtiKgGMGTaoY0xEAuax2EIdLDRONsmfW3QHWgFnqM/view\">https://docs.google.com/document/d/1XSrtiKgGMGTaoY0xEAuax2EIdLDRONsmfW3QHWgFnqM/view</a>",
+	"OPS-RDF": "E. Willighagen. Guidelines for exposing data as RDF in Open PHACTS, Open PHACTS Working Draft 27 March 2012.",
+	"ORCID": "Open Researcher and Contributor Identifier. 22 March 2012. <a href=\"http://www.orcid.org\">http://www.orcid.org</a>",
+	"PAV": "Provenance Authoring and Versioning Ontology. <a href=\"http://code.google.com/p/pav-ontology/\">http://code.google.com/p/pav-ontology/</a>",
+	"PROV-O": "K. Belhajjame, J. Cheney, D. Garijo, T. Lebo, S. Soiland-Reyes, and S. Zednik. The PROV ontology: Model and formal semantics. Working draft, W3C, 13 December 2011. <a href=\"http://www.w3.org/TR/prov-o/\">http://www.w3.org/TR/prov-o/</a>",
+	"RFC2119": "S. Bradner. Key words for use in RFCs to Indicate Requirement Levels. March 1997. Internet RFC 2119. URL: <a href=\"http://www.ietf.org/rfc/rfc2119.txt\">http://www.ietf.org/rfc/rfc2119.txt</a>",
+	"SAMEAS": "Harry Halpin, Patrick J Hayes, James P McCusker, Deborah L McGuinness, and Henry S Thompson. When owl:sameAs Isn’t the Same: An Analysis of Identity in Linked Data. In International Semantic Web Conference (1), Shanghai, China: Springer, p. 305-320.",
+	"TURTLE": "D. Beckett, T. Berners-Lee, E. Prud'hommeaux. Turtle - Terse RDF Triple Language, W3C Working Draft 09 August 2011. <a href=\"http://www.w3.org/TR/turtle/\">http://www.w3.org/TR/turtle/</a>",
+	"VOID": "Describing Linked Datasets with the VoID Vocabulary. W3C Interest Group Note, 3 March 2011. <a href=\"http://www.w3.org/TR/void/\">http://www.w3.org/TR/void/</a>.",
+	"VOIDP": "A Vocabulary for Data and Dataset Provenance. Technical report, EnAKTing Group, ECS, Southampton, 2011. <a href=\"http://www.enakting.org/provenance/voidp/\">http://www.enakting.org/provenance/voidp/</a>"
 }
