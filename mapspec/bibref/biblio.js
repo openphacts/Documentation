d0026b22b217090396c2b410121fde1196ad1f5d
AlasdairGray <A.Gray@cs.man.ac.uk>
2012-06-12 15:11:47 +0100

    Converted and updated dataset metadata section and various other editing throughout the document.

--- mapspec/bibref/biblio.js
+++ mapspec/bibref/biblio.js
@@ -1,6 +1,7 @@
 berjon.biblio = {
 	"DCTERMS": "DCMI Metadata Terms, Dublin Core Metadata Initiative, 11 October 2010. <a href=\"http://dublincore.org/documents/2010/10/11/dcmi-terms/\">http://dublincore.org/documents/2010/10/11/dcmi-terms/</a>",
 	"Chem2Bio2RDF": "B. Chen, X. Dong, D. Jiao, H. Wang, Q. Zhu, Y. Ding, and D.J. Wild. Chem2Bio2RDF: a semantic framework for linking and data mining chemogenomic and systems chemical biology data. BMC Bioinformatics, 11(1):255, 2010.",
+	"FOAF": "FOAF Vocabulary Specification, D. Brickley, L. Miller, 9 August 2010. <a href=\"http://xmlns.com/foaf/spec/20100809.html\">http://xmlns.com/foaf/spec/20100809.html</a>",
 	"Halpin2010": "H. Halpin, P. J. Hayes, J. P. McCusker, D. L. McGuinness, and H. S. Thompson. When owl:sameAs Isn’t the Same: An Analysis of Identity in Linked Data. In International Semantic Web Conference (1), Shanghai, China: Springer, p. 305-320.",
 	"IMS": "Identity Mapping Service, <a href=\"https://wiki.openphacts.org/index.php/IMS_API\">https://wiki.openphacts.org/index.php/IMS_API</a>",
 	"OPM": " ",
@@ -11,7 +12,7 @@ berjon.biblio = {
 	"PAV": "Provenance Authoring and Versioning Ontology. <a href=\"http://code.google.com/p/pav-ontology/\">http://code.google.com/p/pav-ontology/</a>",
 	"PROV-O": "K. Belhajjame, J. Cheney, D. Garijo, T. Lebo, S. Soiland-Reyes, and S. Zednik. The PROV ontology: Model and formal semantics. Working draft, W3C, 13 December 2011. <a href=\"http://www.w3.org/TR/prov-o/\">http://www.w3.org/TR/prov-o/</a>",
 	"RFC2119": "S. Bradner. Key words for use in RFCs to Indicate Requirement Levels. March 1997. Internet RFC 2119. URL: <a href=\"http://www.ietf.org/rfc/rfc2119.txt\">http://www.ietf.org/rfc/rfc2119.txt</a>",
-	"SAMEAS": "Harry Halpin, Patrick J Hayes, James P McCusker, Deborah L McGuinness, and Henry S Thompson. When owl:sameAs Isn’t the Same: An Analysis of Identity in Linked Data. In International Semantic Web Conference (1), Shanghai, China: Springer, p. 305-320.",
+	"SILK": "Silk - A Link Discovery Framework for the Web of Data. <a href=\"http://www4.wiwiss.fu-berlin.de/bizer/silk/\">http://www4.wiwiss.fu-berlin.de/bizer/silk/</a>.",
 	"TURTLE": "D. Beckett, T. Berners-Lee, E. Prud'hommeaux. Turtle - Terse RDF Triple Language, W3C Working Draft 09 August 2011. <a href=\"http://www.w3.org/TR/turtle/\">http://www.w3.org/TR/turtle/</a>",
 	"VOID": "Describing Linked Datasets with the VoID Vocabulary. W3C Interest Group Note, 3 March 2011. <a href=\"http://www.w3.org/TR/void/\">http://www.w3.org/TR/void/</a>.",
 	"VOIDP": "A Vocabulary for Data and Dataset Provenance. Technical report, EnAKTing Group, ECS, Southampton, 2011. <a href=\"http://www.enakting.org/provenance/voidp/\">http://www.enakting.org/provenance/voidp/</a>"
