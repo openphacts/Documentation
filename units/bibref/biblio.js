90cdcb409a7797534960837ce6110465a100757b
Egon Willighagen <egon.willighagen@gmail.com>
2012-11-21 08:34:55 +0100

    Template for the units guidelines

---
+++ units/bibref/biblio.js
@@ -0,0 +1,3 @@
+berjon.biblio = {
+   "Marshall2012" : "Marshall, M. S.;  Boyce, R.;  Deus, H. F.;  Zhao, J.;   Willighagen, E. L.;  Samwald, M.;  Pichler, E.;  Hajagos, J.;   Prud'hommeaux, E.;  Stephens, S. Web Semantics: Science, Services and Agents on the World Wide Web 2012, <a href=\"http://dx.doi.org/10.1016/j.websem.2012.02.003\">http://dx.doi.org/10.1016/j.websem.2012.02.003</a>."
+}
