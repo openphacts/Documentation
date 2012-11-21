bb2ba6caad944097fd9696729fef83b8c9121b12
Egon Willighagen <egon.willighagen@gmail.com>
2012-11-21 08:55:15 +0100

    Written the introduction

--- units/bibref/biblio.js
+++ units/bibref/biblio.js
@@ -1,3 +1,3 @@
 berjon.biblio = {
-   "Marshall2012" : "Marshall, M. S.;  Boyce, R.;  Deus, H. F.;  Zhao, J.;   Willighagen, E. L.;  Samwald, M.;  Pichler, E.;  Hajagos, J.;   Prud'hommeaux, E.;  Stephens, S. Web Semantics: Science, Services and Agents on the World Wide Web 2012, <a href=\"http://dx.doi.org/10.1016/j.websem.2012.02.003\">http://dx.doi.org/10.1016/j.websem.2012.02.003</a>."
+   "Williams2012" : "Williams, A. J., Harland, L., Groth, P., Pettifer, S., Chichester, C., Willighagen, E. L., Evelo, C. T., et al. (2012). Open PHACTS: Semantic interoperability for drug discovery. Drug Discovery Today. <a href=\"http://dx.doi.org/10.1016/j.drudis.2012.05.016\">http://dx.doi.org/10.1016/j.drudis.2012.05.016</a>."
 }
