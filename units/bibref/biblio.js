ae6c6c5abb98b496eac8ffdb90d2cb6ded06e1f0
Egon Willighagen <egon.willighagen@gmail.com>
2013-01-14 18:50:37 +0100

    Added a Java source code example

--- units/bibref/biblio.js
+++ units/bibref/biblio.js
@@ -1,3 +1,4 @@
 berjon.biblio = {
-   "Williams2012" : "Williams, A. J., Harland, L., Groth, P., Pettifer, S., Chichester, C., Willighagen, E. L., Evelo, C. T., et al. (2012). Open PHACTS: Semantic interoperability for drug discovery. Drug Discovery Today. <a href=\"http://dx.doi.org/10.1016/j.drudis.2012.05.016\">http://dx.doi.org/10.1016/j.drudis.2012.05.016</a>."
+   "Williams2012" : "Williams, A. J., Harland, L., Groth, P., Pettifer, S., Chichester, C., Willighagen, E. L., Evelo, C. T., et al. (2012). Open PHACTS: Semantic interoperability for drug discovery. Drug Discovery Today. <a href=\"http://dx.doi.org/10.1016/j.drudis.2012.05.016\">http://dx.doi.org/10.1016/j.drudis.2012.05.016</a>.",
+   "Willighagen2012jQUDT" : "Willighagen, E.L. (2012), <a href=\"http://github.com/egonw/jqudt/\">http://github.com/egonw/jqudt/</a>" 
 }
