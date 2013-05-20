836088e3cd2b560b5d95606b11cba9cf19216645
AlasdairGray <A.Gray@cs.man.ac.uk>
2013-05-20 17:35:09 +0100

    Created document for lens specification

---
+++ lens/bibref/biblio.js
@@ -0,0 +1,11 @@
+berjon.biblio = {
+	"DCTERMS": "DCMI Metadata Terms, Dublin Core Metadata Initiative, 11 October 2010. <a href=\"http://dublincore.org/documents/2010/10/11/dcmi-terms/\">http://dublincore.org/documents/2010/10/11/dcmi-terms/</a>",
+	"FOAF": "FOAF Vocabulary Specification, D. Brickley, L. Miller, 9 August 2010. <a href=\"http://xmlns.com/foaf/spec/20100809.html\">http://xmlns.com/foaf/spec/20100809.html</a>",
+	"IMS": "Identity Mapping Service, <a href=\"https://wiki.openphacts.org/index.php/IMS_API\">https://wiki.openphacts.org/index.php/IMS_API</a>",
+	"OPS-ARCH": "Lee Harland, Paul Groth, Steve Pettifer, Christine Chichester, and Antony Williams. Provide a documented first functional design and a detailed technical design of the first version of OPS. Open PHACTS Deliverable D3.2.3, version 1.0, August 2011.",
+	"OPS-LENSES": "C. Brenninkmeijer, C. Evelo, C. Goble, A. J. G. Gray, P. Groth, S. Pettifer, R. Stevens, A. J. Williams, and E. Willighagen. Scientific Lenses over Linked Data: An approach to support task specific views of the data. A vision. Linked Science Workshop, Boston, MA, USA, 2012. To appear.",
+	"OPS-METADATA": "A. J. G. Gray. Dataset Descriptions for the Open Pharmacological Space, Open PHACTS Working Draft 19 October 2012. <a href=\"http://www.openphacts.org/specs/datadesc/\">http://www.openphacts.org/specs/datadesc/</a>",
+	"OPS-RDF": "E. Willighagen. Guidelines for exposing data as RDF in Open PHACTS, Open PHACTS Working Draft 27 March 2012. <a href=\"http://www.openphacts.org/specs/rdfguide/\">http://www.openphacts.org/specs/rdfguide/</a>",
+	"RFC2119": "S. Bradner. Key words for use in RFCs to Indicate Requirement Levels. March 1997. Internet RFC 2119. URL: <a href=\"http://www.ietf.org/rfc/rfc2119.txt\">http://www.ietf.org/rfc/rfc2119.txt</a>",
+	"TURTLE": "D. Beckett, T. Berners-Lee, E. Prud'hommeaux. Turtle - Terse RDF Triple Language, W3C Working Draft 09 August 2011. <a href=\"http://www.w3.org/TR/turtle/\">http://www.w3.org/TR/turtle/</a>",
+}
