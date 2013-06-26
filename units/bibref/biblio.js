ca546c87fb56cc7c1d1b1b7c7f51d54abdb40af2
Egon Willighagen <egon.willighagen@gmail.com>
2013-06-26 19:00:27 +0200

    Added the Unit Ontology mappings

--- units/bibref/biblio.js
+++ units/bibref/biblio.js
@@ -1,5 +1,6 @@
 berjon.biblio = {
    "Williams2012" : "Williams, A. J., Harland, L., Groth, P., Pettifer, S., Chichester, C., Willighagen, E. L., Evelo, C. T., et al. (2012). Open PHACTS: Semantic interoperability for drug discovery. Drug Discovery Today. <a href=\"http://dx.doi.org/10.1016/j.drudis.2012.05.016\">http://dx.doi.org/10.1016/j.drudis.2012.05.016</a>.",
    "Willighagen2012jQUDT" : "Willighagen, E.L. (2012), <a href=\"http://github.com/egonw/jqudt/\">http://github.com/egonw/jqudt/</a>", 
-   "Willighagen2013" : "Willighagen, E.L., Waagmeester, A., Spjuth, O., Ansell, P., Williams, A.J., Tkachenko, V., Hastings, J., Chen, B., Wild, D.J. (2013). The ChEMBL database as Linked Open Data, J. Cheminformatics. <a href=\"http://dx.doi.org/\">http://dx.doi.org/</a>."
+   "Willighagen2013" : "Willighagen, E.L., Waagmeester, A., Spjuth, O., Ansell, P., Williams, A.J., Tkachenko, V., Hastings, J., Chen, B., Wild, D.J. (2013). The ChEMBL database as Linked Open Data, J. Cheminformatics. <a href=\"http://dx.doi.org/\">http://dx.doi.org/</a>.",
+   "UO2013" : "Unit Ontology (2013). <a href=\"http://code.google.com/p/unit-ontology/\">http://code.google.com/p/unit-ontology/</a>."
 }
