8f208d872d05740792ef55640b959470e3f2c69b
Egon Willighagen <egon.willighagen@gmail.com>
2013-09-13 19:04:30 +0200

    Added the namespaces at the start of the document (suggested by and thanks to Alasdair)

--- units/bibref/biblio.js
+++ units/bibref/biblio.js
@@ -2,5 +2,6 @@ berjon.biblio = {
    "Williams2012" : "Williams, A. J., Harland, L., Groth, P., Pettifer, S., Chichester, C., Willighagen, E. L., Evelo, C. T., et al. (2012). Open PHACTS: Semantic interoperability for drug discovery. Drug Discovery Today. <a href=\"http://dx.doi.org/10.1016/j.drudis.2012.05.016\">http://dx.doi.org/10.1016/j.drudis.2012.05.016</a>.",
    "Willighagen2012jQUDT" : "Willighagen, E.L. (2012), <a href=\"http://github.com/egonw/jqudt/\">http://github.com/egonw/jqudt/</a>", 
    "Willighagen2013" : "Willighagen, E.L., Waagmeester, A., Spjuth, O., Ansell, P., Williams, A.J., Tkachenko, V., Hastings, J., Chen, B., Wild, D.J. (2013). The ChEMBL database as Linked Open Data, J. Cheminformatics. <a href=\"http://dx.doi.org/\">http://dx.doi.org/</a>.",
+   "TURTLE": "D. Beckett, T. Berners-Lee, E. Prud'hommeaux. Turtle - Terse RDF Triple Language, W3C Working Draft 09 August 2011. <a href=\"http://www.w3.org/TR/turtle/\">http://www.w3.org/TR/turtle/</a>",
    "UO2013" : "Unit Ontology (2013). <a href=\"http://code.google.com/p/unit-ontology/\">http://code.google.com/p/unit-ontology/</a>."
 }
