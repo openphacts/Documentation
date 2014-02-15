8dde9f39574cf05c1cfdf6aadf1820feeb009c58
Egon Willighagen <egon.willighagen@gmail.com>
2014-02-15 11:37:48 +0100

    Fixed the title.

--- template/bibref/biblio.js
+++ template/bibref/biblio.js
@@ -1,3 +1,3 @@
 berjon.biblio = {
-   "Marshall2012" : "Marshall, M. S.;  Boyce, R.;  Deus, H. F.;  Zhao, J.;   Willighagen, E. L.;  Samwald, M.;  Pichler, E.;  Hajagos, J.;   Prud'hommeaux, E.;  Stephens, S. Web Semantics: Science, Services and Agents on the World Wide Web 2012, <a href=\"http://dx.doi.org/10.1016/j.websem.2012.02.003\">http://dx.doi.org/10.1016/j.websem.2012.02.003</a>."
+   "Marshall2012" : "Marshall, M. S.;  Boyce, R.;  Deus, H. F.;  Zhao, J.;   Willighagen, E. L.;  Samwald, M.;  Pichler, E.;  Hajagos, J.;   Prud'hommeaux, E.;  Stephens, S. Emerging practices for mapping and linking life sciences data using RDF - A case series, <a href=\"http://dx.doi.org/10.1016/j.websem.2012.02.003\">http://dx.doi.org/10.1016/j.websem.2012.02.003</a>."
 }
